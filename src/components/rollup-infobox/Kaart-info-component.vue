ecdefc44512694f2bbbbbb797f8d597f11a3599f
Werner van Randeraat <werner.van.randeraat@youngcoders.nl>
2019-02-22 15:51:31 +0100

    Rollup box laat nu de content zien behorend bij de 'kaart' optie in het menu.

--- src/components/rollup-infobox/Kaart-info-component.vue
+++ src/components/rollup-infobox/Kaart-info-component.vue
@@ -1,14 +1,77 @@
 <template>
-    <p>Kaart</p>
+    <div class="">
+        <p class="title">Verkeersinformatie voor de binnenvaart</p>
+        <div class="row justify-content-around justify-content-md-center">
+            <info-item v-for="(menuIcon, index) in menuIcons" :key="index" :menu-item="menuIcon"></info-item>
+        </div>
+        <hr>
+        <div class="item" v-for="item in items">
+            {{ item }}
+        </div>
+    </div>
 </template>
 
 <script>
+    import Icon from './Info-icon'
     export default {
-
+        data () {
+            return {
+                items: [
+                    'Autosteiger',
+                    'Bunkerstation',
+                    'Drinkwater',
+                    'Sluizen',
+                    'Haven',
+                    'Restaurants',
+                    'Toiletten',
+                    'Aanleg plaatsen',
+                    'Tank stations'
+                ],
+                menuIcons: {
+                    arrow: {
+                        imagename: 'arrow.svg',
+                        menutext: 'Sluizen'
+                    },
+                    anker: {
+                        imagename: 'anker.svg',
+                        menutext: 'Ligplaats'
+                    },
+                    waterpas: {
+                        imagename: 'waterpas.svg',
+                        menutext: 'Waterstand'
+                    },
+                    fabriek: {
+                        imagename: 'fabriek.svg',
+                        menutext: 'Terminals'
+                    },
+                    afvalbak: {
+                        imagename: 'afvalbak.svg',
+                        menutext: 'Afvalplaats'
+                    }
+                },
+            }
+        },
+        components: {
+            infoItem: Icon
+        },
     }
 
 </script>
 
 <style>
-
+    hr {
+        margin-bottom: .3em;
+    }
+    .item {
+        border-bottom: solid .5px #F0F0F0;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        text-align: left;
+        padding-left: 40px;
+    }
+    .title {
+        font-size: 1.2em;
+        margin: 20px 0px 20px 0px;
+        font-weight: 500;
+    }
 </style>
